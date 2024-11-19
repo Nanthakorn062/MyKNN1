0cfb325635fcc8696aa0b50a65ef06829e38403b
Nanthakorn062 <157331574+Nanthakorn062@users.noreply.github.com>
2024-11-19 15:46:27 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -4,8 +4,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
 
 st.header('Nanthakorn')
 #st.image("./pic/kairung.jpg")
